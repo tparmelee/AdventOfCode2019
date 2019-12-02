0581c68977c98115519f844bd04d5205dc8101e9
Tyler P <t.a.parmelee@gmail.com>
2019-12-01 22:11:05 -0800

    complete day 2 part 2

--- Implementation/Day02/Part2.cs
+++ Implementation/Day02/Part2.cs
@@ -6,6 +6,41 @@ namespace Day02
 {
     public class Part2
     {
-        
+        public static int FindNounVerbToGetOutput(int[] intProgram, int outputValueToFind)
+        {
+
+            for(int noun = 0; noun <=99; noun++)
+            {
+                for (int verb = 0; verb <= 99; verb++)
+                {
+                    int[] working = DeepClone(intProgram);
+                    working = PutProgramIntoState(working, noun, verb);
+                    int[] result = Part1.RunIntProgram(working);
+
+                    if (result[0] == outputValueToFind)
+                        return 100 * noun + verb;
+
+                }
+            }
+
+            return - 1;
+        }
+
+        private static int[] DeepClone(int[] input)
+        {
+            int[] output = new int[input.Length];
+            for(int i = 0; i < input.Length; i++)
+            {
+                output[i] = input[i];
+            }
+            return output;
+        }
+
+        public static int[] PutProgramIntoState(int[] intProgram, int noun, int verb)
+        {
+            intProgram[1] = noun;
+            intProgram[2] = verb;
+            return intProgram;
+        }
     }
 }
