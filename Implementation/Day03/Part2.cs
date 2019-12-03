2d27fc5e0af955bc2daf9ec92148ec457d840128
Tyler P <t.a.parmelee@gmail.com>
2019-12-02 23:24:59 -0800

    complete part 1

---
+++ Implementation/Day03/Part2.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day03
+{
+    public class Part2
+    {
+        
+    }
+}
---
+++ Tests/Day03/Part2_Tests.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+
+namespace Day03
+{
+    public class Part2_Tests
+    {
+
+    }
+}
