108798c5aff73d4ccacfd6616e22e307f65b5e08
Tyler P <t.a.parmelee@gmail.com>
2019-12-03 21:15:39 -0800

    get part 1 working

---
+++ Implementation/Day04/P1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day04
+{
+    public class P1
+    {
+
+        public static bool IsValidPassword(string password)
+        {
+            if (password.Length != 6)
+                return false;
+
+
+            bool adjacentMatchExists = false;
+            for (int i = 0; i < password.Length - 1; i++)
+            {
+                int i0 = int.Parse(password[i].ToString());
+                int i1 = int.Parse(password[i+1].ToString());
+
+                if (i1 < i0)
+                    return false;
+                if (i0 == i1)
+                    adjacentMatchExists = true;
+            }
+
+            return adjacentMatchExists;
+        }
+
+        public static long NumberOfPasswords(string s1, string s2)
+        {
+            long p1 = long.Parse(s1);
+            long p2 = long.Parse(s2);
+
+            long count = 0;
+
+            for(long i = p1; i <= p2; i++)
+            {
+                if (IsValidPassword(i.ToString()))
+                    count++;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ Tests/Day04/Part1_Tests.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+
+namespace Day04
+{
+    public class Part1_Tests
+    {
+
+        [Theory]
+        [InlineData("111111", true)]
+        [InlineData("223450", false)]
+        [InlineData("123789", false)]
+        public void BaseCases(string password, bool isValidExpected)
+        {
+            bool isValid = P1.IsValidPassword(password);
+            Assert.Equal(isValidExpected, isValid);
+        }
+
+        [Fact]
+        public void Part1Answers()
+        {
+            string s1 = "278384";
+            string s2 = "824795";
+
+            long result = P1.NumberOfPasswords(s1, s2);
+
+            Assert.Equal(921, result);
+        }
+
+    
+    }
+}
