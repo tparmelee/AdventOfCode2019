0613f189192b7aa590ea22f7efd6beddbd913224
Tyler P <t.a.parmelee@gmail.com>
2019-12-03 22:51:02 -0800

    complete part 2

---
+++ Implementation/Day04/Part2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Day04
+{
+    public class Part2
+    {
+        public static bool IsValidPassword(string password)
+        {
+            if (password.Length != 6)
+                return false;
+
+            Dictionary<int, int> counts = new Dictionary<int, int>();
+            int prev = -1;
+            for (int i = 0; i < password.Length; i++)
+            {
+                int curVal = int.Parse(password[i].ToString());
+
+                // sorted
+                if (curVal < prev)
+                    return false;
+
+                if (counts.ContainsKey(curVal))
+                    counts[curVal]++;
+                else
+                    counts[curVal] = 1;
+
+                prev = curVal;
+            }
+            
+            bool adjacentMatchExists = false;
+            foreach (var pair in counts)
+            {
+                if (pair.Value == 2)
+                    adjacentMatchExists = true;
+            }
+
+            return adjacentMatchExists;
+        }
+
+        public static int NumberOfPasswords(string s1, string s2)
+        {
+            int p1 = int.Parse(s1);
+            int p2 = int.Parse(s2);
+
+            int count = 0;
+            for (int i = p1; i <= p2; i++)
+            {
+                if (IsValidPassword(i.ToString()))
+                    count++;
+            }
+
+            return count;
+        }
+
+    }
+}
--- Tests/Day04/Part2_Tests.cs
+++ Tests/Day04/Part2_Tests.cs
@@ -9,5 +9,29 @@ namespace Day04
     public class Part2_Tests
     {
 
+        [Theory]
+        [InlineData("112233", true)]
+        [InlineData("123444", false)]
+        [InlineData("123440", false)]
+        [InlineData("124444", false)]
+        [InlineData("111122", true)]
+        [InlineData("122670", false)]
+        public void BaseCases(string password, bool isValidExpected)
+        {
+            
+            bool isValid = Part2.IsValidPassword(password);
+            Assert.Equal(isValidExpected, isValid);
+        }
+
+        [Fact]
+        public void Part2Answers()
+        {
+            string s1 = "278384";
+            string s2 = "824795";
+
+            long result = Part2.NumberOfPasswords(s1, s2);
+
+            Assert.Equal(603, result);
+        }
     }
 }
